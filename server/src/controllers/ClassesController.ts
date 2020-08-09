d4bfc288ab7f89e21a3daab129da4927c980b28f
Vinicius Scavoni de Lima <vinicius.lima@yandeh.com.br>
2020-08-09 11:43:46 -0300

    finalizando metodos

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -27,8 +27,19 @@ export default class ClassesController{
         const timeInMinutes = convertHourToMinuts(time);
         
         const classes = await db('classes')
+            .whereExists(function() {
+                this.select('class_schedule.*')
+                    .from('class_schedule')
+                    .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+                    .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+                    .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+                    .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
+            })
             .where('classes.subject', '=', subject)
-        response.send();
+            .join('users', 'classes.user_id', '=', 'user_id')
+            .select(['classes.*', 'users.*']);
+        
+        return response.json(classes);
     }
 
 
